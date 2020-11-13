ed84588c965f13734a72c3286450fe2a182b5f6d
Jonathan Welham <jwelham@csumb.edu>
2020-11-12 23:43:46 -0800

    Updated Login route/view

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home login page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login Page' });
+});
+
+
+// router.get('/login', function(req, res){
+// 	res.render('login');
+// })
+
+/**
+ * When the user wants to post we must find the user that was created in the
+ * database. If the user does not exist we reload the page and the user tries again,
+*/
+router.post('/login', function(req, res){
+	User.find(req.body.username, (err, data) => {
+		if(!err && data.length === 1){
+			if(req.body.password === data[0].password){
+				req.session.loggedin = true;
+				req.session.username = req.body.username;
+				res.redirect('/');
+				return;
+			} else { //Password is incorrect
+				res.render('login', {error: true, password: req.body.password});
+			}
+		}
+		res.render('login', {error: true, user: req.body.username});
+	});
 });
 
 
